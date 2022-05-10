e9b4832f1b111b0d04027e2d7d217574159e08d8
Roman Kiselev <roman.kiselef@gmail.com>
2022-05-10 18:19:28 +0300

    implementation

---
+++ src/main/java/ru/leetcode/app/t216/CombinationSumIII.java
@@ -0,0 +1,65 @@
+package ru.leetcode.app.t216;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CombinationSumIII {
+    class Solution {
+        private List<List<Integer>> ans;
+
+        /**
+         *Find all valid combinations of k numbers that sum up to n such that the following conditions are true:
+         *
+         * Only numbers 1 through 9 are used.
+         * Each number is used at most once.
+         * Return a list of all possible valid combinations. The list must not contain the same combination twice, and the combinations may be returned in any order.
+         *
+         *
+         *
+         * Example 1:
+         *
+         * Input: k = 3, n = 7
+         * Output: [[1,2,4]]
+         * Explanation:
+         * 1 + 2 + 4 = 7
+         * There are no other valid combinations.
+         * Example 2:
+         *
+         * Input: k = 3, n = 9
+         * Output: [[1,2,6],[1,3,5],[2,3,4]]
+         * Explanation:
+         * 1 + 2 + 6 = 9
+         * 1 + 3 + 5 = 9
+         * 2 + 3 + 4 = 9
+         * There are no other valid combinations.
+         * Example 3:
+         *
+         * Input: k = 4, n = 1
+         * Output: []
+         * Explanation: There are no valid combinations.
+         * Using 4 different numbers in the range [1,9], the smallest sum we can get is 1+2+3+4 = 10 and
+         * since 10 > 1, there are no valid combination.
+         */
+
+        public List<List<Integer>> combinationSum3(int k, int n) {
+            ans = new ArrayList<>();
+            lsf(k, n, new ArrayList<>(), 0);
+            return ans;
+        }
+
+        public void lsf(int k, int n, List<Integer> ls, int start) {
+            if (n < 0) {
+                return;
+            }
+            if (k == 0 && n == 0) {
+                ans.add(new ArrayList<>(ls));
+            } else {
+                for (int i = start + 1; i <= 9; i++) {
+                    ls.add(0, i);
+                    lsf(k - 1, n - i, ls, i);
+                    ls.remove(0);
+                }
+            }
+        }
+    }
+}
