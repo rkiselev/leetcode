c42fcaedfec71f1e1bba4f79d744e1c2ae925ddd
Roman Kiselev <roman.kiselef@gmail.com>
2022-06-10 16:37:21 +0300

    implementation

--- src/main/java/ru/leetcode/app/t853/CarFleet.java
+++ src/main/java/ru/leetcode/app/t853/CarFleet.java
@@ -5,10 +5,13 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Stack;
 
+import static org.junit.Assert.assertEquals;
+
 public class CarFleet {
     public static void main(String[] args) {
-        new Solution().carFleet(10, new int[]{6,8}, new int[]{3, 2});
-        new Solution().carFleet(12, new int[]{10,8,0,5,3}, new int[]{2,4,1,1,3});
+        assertEquals(1, new Solution().carFleet(100, new int[]{0,4,2}, new int[]{2,1,3}));
+        assertEquals(3, new Solution().carFleet(12, new int[]{10,8,0,5,3}, new int[]{2,4,1,1,3}));
+        assertEquals(2, new Solution().carFleet(10, new int[]{6,8}, new int[]{3, 2}));
     }
     static class Solution {
         public int carFleet(int target, int[] position, int[] speed) {
@@ -17,24 +20,22 @@ public class CarFleet {
                 cars.add(new Car(speed[i], position[i]));
             }
             cars.sort(Comparator.comparingInt(a -> a.position));
-            Stack<Car> stack = new Stack<>();
-            for(Car car: cars) {
-                boolean slow = false;
-                while(!stack.isEmpty() && !slow && stack.peek().speed > car.speed) {
-                    float time = (target - car.position) / (car.speed * 1.0f);
-                    float catchUpTime = (target - stack.peek().position) / (stack.peek().speed * 1.0f);
-                    if (time >= catchUpTime) {
-                        stack.pop();
-                    } else {
-                        slow = true;
+            Stack<Float> stack = new Stack<>();
+            for (int i = cars.size() - 1; i >= 0; i--) {
+                Car car = cars.get(i);
+                float time = (target - car.position) / (car.speed * 1.0f);
+                if (!stack.isEmpty()) {
+                    float catchUpTime = stack.peek();
+                    if (time <= catchUpTime) {
+                        time = stack.pop();
                     }
                 }
-                stack.push(car);
+                stack.push(time);
             }
             return stack.size();
         }
 
-        private class Car {
+        private static class Car {
             int speed;
             int position;
             Car(int speed, int position) {
