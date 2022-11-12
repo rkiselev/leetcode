e55c0be1e03fcacb826f4a401054553cb7db5a08
Roman Kiselev <roman.kiselef@gmail.com>
2022-11-12 11:39:03 +0300

    implementation

--- src/main/java/ru/leetcode/app/t7/ReverseInteger.java
+++ src/main/java/ru/leetcode/app/t7/ReverseInteger.java
@@ -23,12 +23,12 @@ public class ReverseInteger {
         boolean revers = x < 0;
         int result = 0;
         x = Math.abs(x);
-        while ( x > 0) {
-            if (result  > Integer.MAX_VALUE / 10) {
+        while (x > 0) {
+            if (result > Integer.MAX_VALUE / 10) {
                 return 0;
             }
             result = result * 10 + x % 10;
-            x = x/10;
+            x = x / 10;
         }
         if (revers) result *= -1;
 
