a8bf9086027f53526747d38cc024449c8146e0a9
Roman Kiselev <roman.kiselef@gmail.com>
2023-03-27 06:57:43 +0300

    implementation

---
+++ src/main/java/ru/leetcode/app/t64/MinimumPathSum.java
@@ -0,0 +1,35 @@
+package ru.leetcode.app.t64;
+
+public class MinimumPathSum {
+    /**
+     * https://leetcode.com/problems/minimum-path-sum/description/
+     * Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right, which minimizes
+     * the sum of all numbers along its path.
+     * <p>
+     * Note: You can only move either down or right at any point in time.
+     */
+
+    class Solution {
+
+        public int minPathSum(int[][] grid) {
+            int m = grid.length;
+            int n = grid[0].length;
+
+            for (int i = 1; i < m; i++) {
+                grid[i][0] += grid[i - 1][0];
+            }
+
+            for (int j = 1; j < n; j++) {
+                grid[0][j] += grid[0][j - 1];
+            }
+
+            for (int i = 1; i < m; i++) {
+                for (int j = 1; j < n; j++) {
+                    grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
+                }
+            }
+
+            return grid[m - 1][n - 1];
+        }
+    }
+}
